f5e5039424c3e25e0dd6d94173a7a796c35c7467
chirag <126777836+chirag713@users.noreply.github.com>
2024-05-14 17:11:35 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ const bar=document.getElementById("bar");
 const nav=document.getElementById("navbar");
 const close=document.getElementById("close");
 
+
+var i = 0;
+var txt = "Welcome to BasicBrush Studios, Your premier partner in story telling and brand elevation.Our passion-filled team specializes in crafting campaigns that delivers tangible results .With expertisein Social media management, web management, and Business management, we ensure that your brands story resonates deeply with your audience, exceeding expectations and driving growth. Join us on this journey of creativity and success.";
+var speed = 50;
+
+let header=document.getElementById("header");
+
+// Call typeWriter function when the page loads
+window.onload = function() {
+  typeWriter();
+};
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+
 if(bar){
     bar.addEventListener("click",()=>{
         nav.classList.add("active");
@@ -12,3 +33,9 @@ if(close){
         nav.classList.remove("active");
     })
 }
+
+window.addEventListener("scroll", () => {
+    let scrollTop = window.scrollY;
+    let headerOpacity = scrollTop > 0 ? 0.9 : 1;
+    header.style.opacity = headerOpacity;
+  });
